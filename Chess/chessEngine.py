e1fd6109056897d19cce5d781ae7992f9aaa96f5
BasemaRehman <basema.rehman@hotmail.co.uk>
2021-06-25 22:42:52 +0100

    Initial commit

---
+++ Chess/chessEngine.py
@@ -0,0 +1,16 @@
+class GameState():
+    def __init__(self):
+        #This is a 2d list - try to implement with numpy arrays
+        #Letter notation is colour and type (N = knight, K = King)
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
---
+++ Chess/chessMain.py
@@ -0,0 +1,52 @@
+import pygame as p
+import chessEngine
+import sys
+#Higher = less resolution
+WIDTH = HEIGHT = 512
+DIMESION = 8
+SQ_SIZE = HEIGHT // DIMESION
+MAX_FPS = 15 #for animations
+IMAGES = {}
+
+def loadImages():
+    pieces = ['wp', 'bp', 'wR', 'bR', 'wB', 'bB', 'wK', 'bK', 'wQ', 'bQ', 'wN', 'bN']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+def main():
+
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = chessEngine.GameState()
+    print (gs.board)
+    loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                p.quit()
+                sys.exit()
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs)
+
+def drawBoard(screen):
+    colours = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMESION):
+        for c in range(DIMESION):
+            colour = colours[((r+c) % 2)]
+            p.draw.rect(screen, colour, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawPieces(screen, gs):
+    pass
+
+
+if __name__ == '__main__':
+    main()
